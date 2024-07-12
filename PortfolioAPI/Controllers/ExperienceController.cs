b0eb3e3129f9fce3fa0199d9288a9e7d8f000ab8
pablopaezsheridan <pablopaezps97@gmail.com>
2024-07-12 17:05:33 -0300

    Agregar archivos de proyecto.

---
+++ PortfolioAPI/Controllers/ExperienceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExperienceController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Hola mundo");
+        }
+    }
+}

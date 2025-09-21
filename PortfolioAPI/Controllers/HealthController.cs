001488f74d574d5a90ac820e8e6c728633abb7bb
pablopaezsheridan <pablopaezps97@gmail.com>
2025-09-21 00:10:58 -0300

    Health Controller

---
+++ PortfolioAPI/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Portfolio API is running");
+        }
+    }
+}
